63984ff835fdd687eab4f19d2455fefad6911049
RedmanPlus <RumataYounger@gmail.com>
2022-07-18 00:38:26 +0300

    experimenting with wsgi

--- src/wsgi.py
+++ src/wsgi.py
@@ -3,4 +3,13 @@ def get_wsgi_data():
 	pass
 
 def app(environ, start_response):
-	pass
+	data = b'Hey'
+	status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    print(environ)
+    print(start_response)
+    start_response(status, response_headers)
+    return iter([data])
--- test.py
+++ test.py
@@ -1,10 +1,36 @@
-def app(environ, start_response):
-    """Simplest possible application object"""
-    data = b'Hello, World!\n'
-    status = '200 OK'
-    response_headers = [
-        ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
-    start_response(status, response_headers)
-    return iter([data])
+class WSGI:
+
+    def __init__(self) -> None:
+        self._data = None
+        self._status = None
+        self._response_headers = None
+
+    def __call__(self, environ, start_response) -> any:
+        start_response(self._status, self._response_headers)
+
+        return iter([self._data])
+
+    def _set_data(self, new_data: bytes) -> None:
+        self._data = new_data
+
+    def _set_status(self, new_status: str) -> None:
+        self._status = new_status
+
+    def _set_headers(self, headers: list) -> None:
+        self._response_headers = headers 
+
+    def set(self, response: dict) -> None:
+        self._set_data(response['data'])
+        self._set_status(response['status'])
+        self._set_headers(response['headers'])
+
+test = WSGI()
+
+test.set({
+        'data': b'Hi mom',
+        'status': '200 OK',
+        'headers': [
+            ('Content-type', 'text/plain'),
+            ('Content-Length', '6')
+    ]})
+
